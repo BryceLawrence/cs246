67988cf5f2bb6f77b315612e8840f74f6aad5435
BryceLawrence <bryce.lawrence@gmail.com>
2014-10-11 21:56:13 -0600

    Going back

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,7 @@ public class HelloWorld {
    public static void main(String[] args) {
 
 
-      System.out.println("Hello GitHub");
-      System.out.println("Testing...");
+      System.out.println("Hello GitHub World");
 
    }
 }
